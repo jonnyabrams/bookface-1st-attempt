06c8b08726662ca4c05cc62698d2419830f731e0
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-07 17:59:44 +0100

    Install bcrypt and hash passwords

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,17 +1,21 @@
 import express from 'express'
 import User from '../models/UserModel.js'
+import bcrypt from 'bcrypt'
 
 const router = express.Router()
 
 // register
 router.post('/register', async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  })
-
   try {
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    })
+
     const user = await newUser.save()
     res.status(200).json(user)
   } catch (error) {
