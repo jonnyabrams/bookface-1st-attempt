d7efaf94df91b3501fc1451f1bf118a381606ac8
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-07 15:01:09 +0100

    Connect to MongoDB database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import helmet from 'helment'
+import helmet from 'helmet'
 import morgan from 'morgan'
 import 'dotenv/config'
 
 const app = express()
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log("Connected to database")
+})
 
 app.listen(8000, () => {
   console.log("Server is running")
