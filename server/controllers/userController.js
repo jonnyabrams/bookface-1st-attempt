0b317d25d4b5a6deb255a3d77e81e3b20f6cf436
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-08 09:05:46 +0100

    Control which user details are returned with getUser

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -42,7 +42,8 @@ export const deleteUser = async (req, res) => {
 export const getUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id)
-    res.status(200).json(user)
+    const { password, updatedAt, ...others } = user._doc
+    res.status(200).json(others)
   } catch (error) {
     res.status(500).json(error)
   }
