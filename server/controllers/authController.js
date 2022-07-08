9f5e506a544665a09e448e455266cc7e5fc9d832
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-08 08:20:01 +0100

    Refactored routes logic into controllers

---
+++ server/controllers/authController.js
@@ -0,0 +1,34 @@
+import User from '../models/UserModel.js'
+import bcrypt from 'bcrypt'
+
+export const registerUser = async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    })
+
+    const user = await newUser.save()
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+export const loginUser = async (req, res) => {
+  try {
+    const user = await User.findOne({email: req.body.email})
+    !user && res.status(404).send("User not found")
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("Wrong password")
+
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,42 +1,9 @@
 import express from 'express'
-import User from '../models/UserModel.js'
-import bcrypt from 'bcrypt'
+import { loginUser, registerUser } from '../controllers/authController.js'
 
 const router = express.Router()
 
-// register
-router.post('/register', async (req, res) => {
-  try {
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(req.body.password, salt)
-
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword
-    })
-
-    const user = await newUser.save()
-    res.status(200).json(user)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-})
-
-// login
-
-router.post('/login', async (req, res) => {
-  try {
-    const user = await User.findOne({email: req.body.email})
-    !user && res.status(404).send("User not found")
-
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).json("Wrong password")
-
-    res.status(200).json(user)
-  } catch (error) {
-    res.status(500).json(error)
-  }
-})
+router.post('/register', registerUser)
+router.post('/login', loginUser)
 
 export default router
