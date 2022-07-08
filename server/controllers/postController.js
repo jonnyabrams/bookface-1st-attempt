12d4d69c6569df2ae71b979ec9460a6e595a1350
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-08 10:44:43 +0100

    createPost route and controller

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,2 +1,12 @@
 import Post from '../models/PostModel.js'
 
+export const createPost = async (req, res) => {
+  const newPost = new Post(req.body)
+
+  try {
+    const savedPost = await newPost.save()
+    res.status(200).json(savedPost)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import { createPost } from '../controllers/postController.js'
 
 const router = express.Router()
 
+router.post('/', createPost)
 
 export default router
