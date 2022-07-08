344b6ba9a4e8c703e7f85750a03b2b66cdf4d2cf
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-08 09:03:30 +0100

    getUser route and controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -38,3 +38,12 @@ export const deleteUser = async (req, res) => {
     res.status(403).json("You can only delete your own account")
   }
 }
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { deleteUser, updateUser } from '../controllers/userController.js'
+import { deleteUser, getUser, updateUser } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser)
+router.get('/:id', getUser)
 
 export default router
