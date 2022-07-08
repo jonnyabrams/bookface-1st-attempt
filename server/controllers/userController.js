2ee0bb95346d27bcbbd5e3378dda2dfa2f200210
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-08 08:46:32 +0100

    updateUser route and controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,27 @@
+import User from '../models/UserModel.js'
+import bcrypt from 'bcrypt'
+
+export const updateUser = async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    // if user tries to update password, generate a new one
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10)
+        req.body.password = await bcrypt.hash(req.body.password, salt)
+      } catch (error) {
+        res.status(500).json(error)
+      }
+    }
+
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+      })
+      res.status(200).json('Account updated')
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  } else {
+    res.status(403).json("You can only update your own account")
+  }
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,16 @@
 import express from 'express'
+import { updateUser } from '../controllers/userController.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send("This is user route")
-})
+router.put('/:id', updateUser)
+
+// delete user 
+
+// get a user
+
+// follow user
+
+// unfollow user
 
 export default router
