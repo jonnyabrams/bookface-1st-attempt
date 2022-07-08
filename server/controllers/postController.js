9b5cf7c32a8c5b255081058ca983777a92932c78
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-08 14:21:58 +0100

    likePost route and controller

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -40,3 +40,19 @@ export const deletePost = async (req, res) => {
     res.status(500).json(error)
   }
 }
+
+export const likePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } })
+      res.status(200).json("Post liked!")
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } })
+      res.status(200).json("Post unliked")
+    }   
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createPost, deletePost, updatePost } from '../controllers/postController.js'
+import { createPost, deletePost, likePost, updatePost } from '../controllers/postController.js'
 
 const router = express.Router()
 
 router.post('/', createPost)
 router.put('/:id', updatePost)
 router.delete('/:id', deletePost)
+router.put('/:id/like', likePost)
 
 export default router
