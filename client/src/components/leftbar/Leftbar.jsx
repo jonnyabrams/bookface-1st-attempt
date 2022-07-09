1d259146dcf6955a198f00138566ec0dc26ba795
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-09 10:09:21 +0100

    Arrange leftbar, feed and rightbar in columns on Home page

---
+++ client/src/components/leftbar/Leftbar.jsx
@@ -0,0 +1,11 @@
+import './leftbar.css'
+
+const Leftbar = () => {
+  return (
+    <div className='leftbar'>
+      Leftbar
+    </div>
+  )
+}
+
+export default Leftbar
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,19 @@
+import Feed from '../../components/feed/Feed'
+import Leftbar from '../../components/leftbar/Leftbar'
 import Topbar from '../../components/topbar/Topbar'
+import Rightbar from '../../components/rightbar/Rightbar'
 import './home.css'
 
 const Home = () => {
   return (
-    <Topbar />
+    <>
+      <Topbar />
+      <div className="home-container">
+        <Leftbar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
   )
 }
 
