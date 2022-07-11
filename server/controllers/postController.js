b3286a5490f1aa1a70890c580fa122c3d91dc7ae
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-11 17:54:42 +0100

    Change post route/controller to take userId in params

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -69,7 +69,7 @@ export const getPost = async (req, res) => {
 
 export const getTimelinePosts = async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId)
+    const currentUser = await User.findById(req.params.userId)
     const userPosts = await Post.find({ userId: currentUser._id })
     const friendPosts = await Promise.all(
       currentUser.following.map((friendId) => {
--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -8,6 +8,6 @@ router.put('/:id', updatePost)
 router.delete('/:id', deletePost)
 router.put('/:id/like', likePost)
 router.get('/:id', getPost)
-router.get('/timeline/all', getTimelinePosts)
+router.get('/timeline/:userId', getTimelinePosts)
 
 export default router
