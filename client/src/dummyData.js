b17f89b40c2e25dc179898ac4714ea5df3aadd68
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-11 08:52:34 +0100

    Create dummy data

---
+++ client/src/dummyData.js
@@ -0,0 +1,56 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: 'assets/profile/1.webp',
+    username: 'Todd Bonzalez'
+  },
+  {
+    id: 2,
+    profilePicture: 'assets/profile/2.jpeg',
+    username: 'Bobson Dugnutt'
+  },
+  {
+    id: 3,
+    profilePicture: 'assets/profile/3.webp',
+    username: 'Dwigt Rortugal'
+  },
+]
+
+export const Posts = [
+  {
+    id: 1,
+    content: "Hello there",
+    img: "assets/stock/1.jpeg",
+    date: "5 mins ago",
+    userId: 2,
+    likes: 2,
+    comments: 2
+  },
+  {
+    id: 2,
+    content: "Oi oi",
+    img: "assets/stock/2.jpeg",
+    date: "10 mins ago",
+    userId: 1,
+    likes: 2,
+    comments: 2
+  },
+  {
+    id: 3,
+    content: "Alright",
+    img: "assets/stock/3.jpeg",
+    date: "15 mins ago",
+    userId: 3,
+    likes: 2,
+    comments: 2
+  },
+  {
+    id: 4,
+    content: "Yo",
+    img: "assets/stock/4.jpeg",
+    date: "20 mins ago",
+    userId: 1,
+    likes: 22,
+    comments: 25
+  }
+]
