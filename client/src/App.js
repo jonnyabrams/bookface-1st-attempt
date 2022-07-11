e83aad279dc99a53bc9946bda059b39505c6b723
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-11 10:05:45 +0100

    Set up Profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import Home from "./pages/home/Home"
+import Profile from "./pages/profile/Profile"
 
 const App = () => {
   return (
-    <Home />
+    <Profile />
   )
 }
 
---
+++ client/src/pages/profile/Profile.jsx
@@ -0,0 +1,34 @@
+import Feed from '../../components/feed/Feed'
+import Leftbar from '../../components/leftbar/Leftbar'
+import Rightbar from '../../components/rightbar/Rightbar'
+import Topbar from '../../components/topbar/Topbar'
+import './profile.css'
+
+const Profile = () => {
+  return (
+    <>
+      <Topbar />
+      <div className="profile">
+        <Leftbar />
+        <div className="profile-right">
+          <div className="profile-right-top">
+            <div className="profile-cover">
+              <img className='profile-cover-image' src="assets/stock/6.jpeg" alt="" />
+              <img className='profile-user-image' src="assets/profile/2.jpeg" alt="" />
+            </div>
+            <div className="profile-info">
+              <h4 className='profile-info-name'>Bobson Dugnutt</h4>
+              <span className='profile-info-description'>This be Bobson</span>
+            </div>
+          </div>
+          <div className="profile-right-bottom">
+            <Feed />
+            <Rightbar />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Profile
