008f57a725babe711042cfeb30dea447ac97d8aa
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-11 18:32:26 +0100

    Fetch post/user data for Feed/Post

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useState } from 'react'
 import Post from '../post/Post'
 import Share from '../share/Share'
 import './feed.css'
-import { Posts } from '../../dummyData'
+import axios from 'axios'
 
 const Feed = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get('post/timeline/62c71599665c278f8a244bce')
+      setPosts(res.data)
+    }
+    fetchPosts()
+  }, [])
+
   return (
     <div className='feed'>
       <div className="feed-wrapper">
         <Share />
-        {Posts.map((p) => (
+        {posts.map((p) => (
           <Post key={p.id} post={p} />
         ))}
       </div>
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,12 +1,21 @@
 import './post.css'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
-import { Users } from '../../dummyData'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Post = ({ post }) => {
   const [likes, setLikes] = useState(post.likes)
   const [isLiked, setIsLiked] = useState(false)
+  const [user, setUser] = useState({})
   const PF = process.env.REACT_APP_PUBLIC_FOLDER
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`user/${post.userId}`)
+      setUser(res.data)
+    }
+    fetchUser()
+  }, [])
 
   const likeHandler = () => {
     setLikes(isLiked ? likes - 1 : likes + 1)
@@ -18,8 +27,8 @@ const Post = ({ post }) => {
       <div className="post-wrapper">
         <div className="post-top">
           <div className="post-top-left">
-            <img className='post-profile-image' src={Users.filter(u => u.id === post.userId)[0].profilePicture} alt="" />
-            <span className="post-username">{Users.filter(u => u.id === post.userId)[0].username}</span>
+            <img className='post-profile-image' src={user.profilePicture} alt="" />
+            <span className="post-username">{user.username}</span>
             <span className="post-date">{post.date}</span>
           </div>
           <div className="post-top-right">
