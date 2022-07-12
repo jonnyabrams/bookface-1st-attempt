5076b61dbae3bd93263ac1b2d30ffdaa04dc7e43
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-12 16:20:07 +0100

    Most recent posts appear first on Feed

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -14,7 +14,9 @@ const Feed = ({ username }) => {
       const res = username 
         ? await axios.get("/post/profile/" + username) 
         : await axios.get('/post/timeline/' + user._id)
-      setPosts(res.data)
+      setPosts(res.data.sort((p1, p2) => {
+        return new Date(p2.createdAt) - new Date(p1.createdAt)
+      }))
     }
     fetchPosts()
   }, [username, user._id])
