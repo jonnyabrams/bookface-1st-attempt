b5d3eb802713a3f9a597a8aabd2491f4e3fd9a02
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-12 14:26:06 +0100

    Fetch timeline posts now dynamic based on user

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import Post from '../post/Post'
 import Share from '../share/Share'
 import './feed.css'
 import axios from 'axios'
+import { AuthContext } from '../../context/AuthContext'
 
 const Feed = ({ username }) => {
   const [posts, setPosts] = useState([])
+  const { user } = useContext(AuthContext)
 
   useEffect(() => {
     const fetchPosts = async () => {
       const res = username 
         ? await axios.get("/post/profile/" + username) 
-        : await axios.get('/post/timeline/62c71599665c278f8a244bce')
+        : await axios.get('/post/timeline/' + user._id)
       setPosts(res.data)
     }
     fetchPosts()
-  }, [username])
+  }, [username, user._id])
 
   return (
     <div className='feed'>
